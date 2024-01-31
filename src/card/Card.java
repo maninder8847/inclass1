0d8a001ea1b55a3c5a7691eb7e673510e2c997ae
manin <manin@10.48.19.148>
2024-01-31 18:29:04 -0500

    exercise

--- src/card/Card.java
+++ src/card/Card.java
@@ -1,30 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package card;
+package Card;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author srinivsi
+ * @author dancye
+ * @author Paul Bonenfant May 2020
  */
 public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
+   
+       public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+    Card() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     /**
      * @return the suit
      */
     public String getSuit() {
         return suit;
     }
+
+
 
     /**
      * @param suit the suit to set
@@ -46,7 +54,4 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
-    
 }
